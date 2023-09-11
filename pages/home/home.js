e0c772a479356a5110d6a4d739ccf179cfaf2de9
handongqingxue <1417140290@qq.com>
2023-09-11 15:23:46 +0800

    1

--- pages/home/home.js
+++ pages/home/home.js
@@ -16,14 +16,14 @@ Page({
    */
   onLoad(options) {
     home=this;
-
+    rootIP=getApp().getRootIP();
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady() {
-    home.getUserInfo();
+    home.login();
   },
 
   /**
@@ -67,21 +67,67 @@ Page({
   onShareAppMessage() {
 
   },
-  getUserInfo: function(e) { 
+  login: function(e) {
     //https://pythonjishu.com/wirvsmuzfagvzej/
     //https://www.zhihu.com/question/272220287/answer/2825656622
+    //https://www.xuexiareas.com/index/Articles/details/num/603.html
+    //https://www.5axxw.com/questions/simple/p9di6v
     wx.login({ 
-      success: function(res) { 
-        if (res.code) { // 发送 res.code 到后台换取 openId, sessionKey, unionId 
-          wx.getUserInfo({ 
-            withCredentials: true, 
-            success: function(res) { 
-              console.log(res.userInfo) 
-            } 
-          }) 
+      success: function(res) {
+        console.log("code="+res.code)
+        if (res.code) { // 发送 res.code 到后台换取 openId, sessionKey, unionId
+          wx.request({
+            url: 'https://api.weixin.qq.com/sns/jscode2session',
+            data: {
+              appid: 'wxa81e31805c520754',
+              secret: '08eb5c17224c178d500c2ac441494cd8',
+              js_code: res.code,
+              grant_type: 'authorization_code'
+            },
+            success:function(res) {
+              console.log(res)
+              const openid = res.data.openid; // 用户的openid
+              const sessionKey = res.data.session_key; // 会话密钥
+              console.log(openid, sessionKey);
+              home.setData({openId:openid});
+              home.getUserInfo();
+            }
+          })
         } 
       } 
     }) 
+  },
+  getUserInfo: function(e) { 
+    wx.getUserInfo({ 
+      withCredentials: true, 
+      success: function(res) { 
+        var userInfo=res.userInfo;
+        console.log(userInfo) 
+        home.setData({nickName:userInfo.nickName,avatarUrl:userInfo.avatarUrl});
+        home.addOrEditWXUser();
+      } 
+    }) 
+  },
+  addOrEditWXUser: function() {
+    let openId=home.data.openId;
+    let nickName=home.data.nickName;
+    let avatarUrl=home.data.avatarUrl;
+    console.log("openId==="+openId)
+    console.log("nickName==="+nickName)
+    console.log("avatarUrl==="+avatarUrl)
+    wx.request({
+      url: rootIP+"addOrEditWXUser",
+      data:{openId:openId,nickName:nickName,avatarUrl:avatarUrl},
+      method: 'POST',
+      header: {
+        'content-type': 'application/x-www-form-urlencoded',
+      },
+      success: function (res) {
+        let data=res.data;
+        let message=data.message;
+        console.log("message==="+message)
+      }
+    })
   },
   goPage:function(e){
     let pageFlag=e.currentTarget.dataset.pageflag;
